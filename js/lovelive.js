ebb4b9ce6cbe0a767c4978eff455b0b14bfdc0f2
wyf <956895214@qq.com>
2015-06-17 02:00:01 +0900

    增加了起始动画效果

--- js/lovelive.js
+++ js/lovelive.js
@@ -2,4 +2,12 @@ $(function(){
 	$('#dowebok').fullpage({
 	'controlArrowColor': '#C0C0C0'
 	});
+
+	$('.start0').fadeOut(2000,picfadein);
+
 });
+
+
+function picfadein(){
+	$('.pic000').fadeIn(1000);
+}
