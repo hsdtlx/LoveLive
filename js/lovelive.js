9080cd956f83cce38b1ba7dcb0438cce30b302ad
wyf <956895214@qq.com>
2015-06-18 01:37:06 +0900

    今天也是在死线上挣扎。。。

--- js/lovelive.js
+++ js/lovelive.js
@@ -1,13 +1,27 @@
 $(function(){
 	$('#dowebok').fullpage({
-	'controlArrowColor': '#C0C0C0'
+	'controlArrowColor': '#C0C0C0',
+
+	afterSlideLoad: function(anchorLink, index, slideIndex, direction){
+			if((index == 4) && (slideIndex == 1)){
+				$('.pjt10').fadeOut(500,pjt11);
+			}
+		}
 	});
 
+	
+
+
 	$('.start0').fadeOut(2000,picfadein);
+
 
 });
 
 
 function picfadein(){
 	$('.pic000').fadeIn(1000);
-}
+};
+
+function pjt11(){
+	$('.pjt11').fadeIn(500);
+};
