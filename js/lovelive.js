46c79afe9b0c026720ff314bc031af00134a64e7
wyf <956895214@qq.com>
2015-06-19 03:47:25 +0900

    算是勉强能看吧。。

--- js/lovelive.js
+++ js/lovelive.js
@@ -2,18 +2,73 @@ $(function(){
 	$('#dowebok').fullpage({
 	'controlArrowColor': '#C0C0C0',
 
+
+	afterLoad: function(anchorLink, index){
+			if(index == 6){
+				$('.fin0').fadeOut(2000,fin);
+			}
+	},
+
 	afterSlideLoad: function(anchorLink, index, slideIndex, direction){
 			if((index == 4) && (slideIndex == 1)){
-				$('.pjt10').fadeOut(500,pjt11);
+				$('.font041').animate({paddingTop:'20px'},pjt01)
+			}
+			if((index == 4) && (slideIndex == 2)){
+				$('.font042').animate({paddingTop:'20px'},pjt02)
+			}
+			if((index == 4) && (slideIndex == 3)){
+				$('.font043').animate({paddingTop:'20px'},pjt03)
+			}
+			if((index == 4) && (slideIndex == 4)){
+				$('.font044').animate({paddingTop:'20px'},pjt04)
+			}
+			if((index == 4) && (slideIndex == 5)){
+				$('.font045').animate({paddingTop:'20px'},pjt05)
+			}
+			if((index == 5) && (slideIndex == 1)){
+				$('.ajitext').fadeIn(2000);
 			}
 		}
 	});
 
-	
-
-
 	$('.start0').fadeOut(2000,picfadein);
 
+	$('.crc10').click(function(){
+		$('.crc11').fadeToggle(500);
+		$('.crc12').fadeToggle(500);
+	});
+	$('.crc20').click(function(){
+		$('.crc21').fadeToggle(500);
+		$('.crc22').fadeToggle(500);
+	});
+	$('.crc30').click(function(){
+		$('.crc31').fadeToggle(500);
+		$('.crc32').fadeToggle(500);
+	});
+	$('.crc40').click(function(){
+		$('.crc41').fadeToggle(500);
+		$('.crc42').fadeToggle(500);
+	});
+	$('.crc50').click(function(){
+		$('.crc51').fadeToggle(500);
+		$('.crc52').fadeToggle(500);
+	});
+	$('.crc60').click(function(){
+		$('.crc61').fadeToggle(500);
+		$('.crc62').fadeToggle(500);
+	});
+	$('.crc70').click(function(){
+		$('.crc71').fadeToggle(500);
+		$('.crc72').fadeToggle(500);
+	});
+	$('.crc80').click(function(){
+		$('.crc81').fadeToggle(500);
+		$('.crc82').fadeToggle(500);
+	});
+	$('.crc90').click(function(){
+		$('.crc91').fadeToggle(500);
+		$('.crc92').fadeToggle(500);
+	});
 
 });
 
@@ -22,6 +77,26 @@ function picfadein(){
 	$('.pic000').fadeIn(1000);
 };
 
-function pjt11(){
-	$('.pjt11').fadeIn(500);
-};
+function pjt01(){
+	$('.pjt01').fadeIn(500);
+}
+
+function pjt02(){
+	$('.pjt02').fadeIn(500);
+}
+
+function pjt03(){
+	$('.pjt03').fadeIn(500);
+}
+
+function pjt04(){
+	$('.pjt04').fadeIn(500);
+}
+
+function pjt05(){
+	$('.pjt05').fadeIn(500);
+}
+
+function fin(){
+	$('.fin').fadeIn(2000);
+}
